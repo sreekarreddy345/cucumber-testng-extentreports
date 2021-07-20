e57d0ca7123e4cda8242e11fb45386b9c1f2576e
Sreekar Reddy <sreekarreddy345@gmail.com>
2021-07-20 13:02:49 -0500

    - Updated extentreports-cucumber6-adapter version. - Updated extent.properties to create reports with date and time.

--- src/main/java/myrunner/TestRunner.java
+++ src/main/java/myrunner/TestRunner.java
@@ -10,7 +10,7 @@ import org.testng.annotations.DataProvider;
         features = "src/main/java/features",
         glue = {"parallel"},
         plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "timeline:test-output-thread/"})
+        })
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
